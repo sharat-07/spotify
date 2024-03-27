3377c71ecd22c4a3912c75430257c06ccfa3232b
Sharat-7 <127922480+Sharat-7@users.noreply.github.com>
2024-03-27 22:46:53 +0530

    check

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ex nostrum porro adipisci tenetur, omnis, quis culpa perspiciatis sunt quibusdam possimus inventore assumenda voluptatibus accusantium, placeat quos ab alias dolor?
+      </p>
     </div>
   );
 }
